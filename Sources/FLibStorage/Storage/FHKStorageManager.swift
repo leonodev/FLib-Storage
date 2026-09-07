c7742e996036410d541b9de5e77b962226570997
Fredy Leon <fredy.leon@ua.la.com>
2026-09-07 15:06:53 +0200

    add test variant

--- Sources/FLibStorage/Storage/FHKStorageManager.swift
+++ Sources/FLibStorage/Storage/FHKStorageManager.swift
@@ -27,3 +27,21 @@ public struct FHKStorageManager: Sendable {
 
     public init() {}
 }
+
+#if DEBUG
+public extension FHKStorageManager {
+    
+    static var test: Self {
+        var manager = Self()
+        let defaultLanguageData = try? JSONEncoder().encode("EN")
+        
+        // set language read from user
+        manager.readUserDefaultsData = { _ in
+            return defaultLanguageData
+        }
+        
+        return manager
+    }
+}
+
+#endif
